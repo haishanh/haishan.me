ed56aaaa245cf878310853600ffefe84e307635a
Haishan <hanhaishan1989@gmail.com>
2018-01-11 23:25:36 +0800

    page: add 404

---
+++ src/pages/404.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import TitleAndMetaTags from '../components/TitleAndMetaTags';
+import createOgUrl from '../utils/createOgUrl';
+import constants from '../constants';
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+import '../utils/reset.scss';
+import style from './404.module.scss';
+
+class Page extends Component {
+  render() {
+    return (
+      <div className={style.root}>
+        <Header />
+        <TitleAndMetaTags
+          title={'Notes - ' + constants.siteName}
+          ogUrl={createOgUrl()}
+        />
+        <div className={style.content}>
+          <h1>404</h1>
+          <h2>Page Not Found</h2>
+          <img
+            src="http://me-1254133903.cossh.myqcloud.com/grinning-face-with-one-large-and-one-small-eye_1f92a.png"
+            alt="404"
+          />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Page;
+
